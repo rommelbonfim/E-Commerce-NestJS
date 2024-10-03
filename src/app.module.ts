4441a92a7f53c25e4821def2880707410d11262a
rommelbonfim <70414415+rommelbonfim@users.noreply.github.com>
2024-10-03 10:55:32 -0300

    exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
 import { ProductModule } from "./product/product.module";
 import { OrderModule } from './order/order.module';
+import { APP_FILTER } from "@nestjs/core";
+import { GlobalExceptionFilter } from "./filters/global-exception-filter";
 
 
 @Module({
@@ -18,6 +20,12 @@ import { OrderModule } from './order/order.module';
       inject: [PostgresConfigService],
     }),
     OrderModule,
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
   ],
 })
 export class AppModule {}
